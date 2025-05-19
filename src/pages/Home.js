74040c379ee065068bc674b68ed99524f8e3400c
brooklyn-lyndontonga <brooklynlt24@gmail.com>
2025-05-19 19:37:46 +1200

    switching up the css

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,28 +6,23 @@ function Home() {
     <>
       <Header />
       <main>
-    <div>
+    <div class='container'>
       <h1>Nau Mai!</h1>
       <h2>Welcome to the virtual home of Funny Looking Face Paints</h2>
       <p>Where we make the funniest faces in Tairawhiti.</p>
       <p>Bookings are now open, so secure your spot now!</p>
     </div>
-    <div>
+    <div class='container'>
       {/* about */}
         <h2>Meet the Funny Fullas Behind the Funny Face Paintings</h2>
         <p>Learn about what makes us Funny! <a href="/src/pages/About">click here</a></p>
     </div>
-    <div>
+    <div class='container'>
       {/* booking */}
         <h2>Book Us For Your Next Party</h2>
         <p>Chech out our <a href="/src/pages/Calendar">Calendar</a> for available dates</p>
     </div>
-    <div>
-      {/* contact */}
-      <h2>Get in Touch</h2>
-      <p></p>
-    </div>
-    <div>
+    <div class='container'>
       {/* gallery */}
       <h2>Checkout Some of Our Work</h2>
       <img src="/public/content/placeholder.jpeg" alt="Placeholder" />

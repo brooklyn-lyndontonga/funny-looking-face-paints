54e483035ec2b9c9650aba069437216d6e8ee992
brooklyn-lyndontonga <brooklynlt24@gmail.com>
2025-05-26 16:35:14 +1200

    calendar visuals

--- src/pages/Calendar.js
+++ src/pages/Calendar.js
@@ -1,20 +1,20 @@
 import React from "react";
+import "../stylesheets/Calendar.css";
 
 function Calendar() {
   return (
-    <div>
+    <div className="container">
       <h1>Calendar</h1>
       <p>Check out the calendar for available dates</p>
-    <iframe
-      title="Google Calendar"
-      src="https://calendar.google.com/calendar/embed?height=600&wkst=1&ctz=Pacific%2FAuckland&showPrint=0&showTz=0&showCalendars=0&showTitle=0&src=ZnVubnlsb29raW5nNDAxMEBnbWFpbC5jb20&color=%23cc89c2"
-      style={{
-        borderWidth: 0,
-      }}
-      width="600"
-      height="400"
-    ></iframe>
-   </div>
+      <div className="calendar-embed-wrapper">
+        <iframe
+          title="Google Calendar"
+          src="https://calendar.google.com/calendar/embed?height=600&wkst=1&ctz=Pacific%2FAuckland&showPrint=0&showTz=0&showCalendars=0&showTitle=0&src=ZnVubnlsb29raW5nNDAxMEBnbWFpbC5jb20&color=%23cc89c2"
+          frameBorder="0"
+          allowFullScreen
+        ></iframe>
+      </div>
+    </div>
   );
 }
 

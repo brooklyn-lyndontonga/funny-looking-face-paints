833439ebf3b763919c5de7f4f5ecb8b3e52408f0
brooklyn-lyndontonga <brooklynlt24@gmail.com>
2025-05-26 16:08:58 +1200

    socials css to remove bulletpoints

--- src/components/Socials.js
+++ src/components/Socials.js
@@ -1,10 +1,11 @@
 import React from "react";
+import "../stylesheets/Socials.css";
 
 function Socials() {
   return (
     <div>
       <h1>Follow The Funny Looking Socials</h1>
-      <ul>
+      <ul className='socials-list'>
         <li><a href="https://www.instagram.com/funnylookingfacepaints/" target="_blank" rel="noopener noreferrer">Instagram</a><icon></icon></li>
         <li><a href="https://www.tiktok.com/@funnylookingfacep?is_from_webapp=1&sender_device=pc" target="_blank" rel="noopener noreferrer">TikTok</a></li>
         <li><a href="https://www.facebook.com/profile.php?id=61576253596992/" target="_blank" rel="noopener noreferrer">Facebook</a></li>

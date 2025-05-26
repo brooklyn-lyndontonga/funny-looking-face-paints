bb1e21f5db19a6413ccb899479c06f2249954c41
brooklyn-lyndontonga <brooklynlt24@gmail.com>
2025-05-26 16:15:01 +1200

    fixes

--- src/components/Socials.js
+++ src/components/Socials.js
@@ -6,7 +6,7 @@ function Socials() {
     <div>
       <h1>Follow The Funny Looking Socials</h1>
       <ul className='socials-list'>
-        <li><a href="https://www.instagram.com/funnylookingfacepaints/" target="_blank" rel="noopener noreferrer">Instagram</a><icon></icon></li>
+        <li><a href="https://www.instagram.com/funnylookingfacepaints/" target="_blank" rel="noopener noreferrer">Instagram</a></li>
         <li><a href="https://www.tiktok.com/@funnylookingfacep?is_from_webapp=1&sender_device=pc" target="_blank" rel="noopener noreferrer">TikTok</a></li>
         <li><a href="https://www.facebook.com/profile.php?id=61576253596992/" target="_blank" rel="noopener noreferrer">Facebook</a></li>
       </ul>
